471e0d56e92b15236184ecbb59e61dcad45f9dbe
Väinö Karppi <vaino.karppi@gmail.com>
2024-01-08 11:20:41 +0200

    ADDED: Automated year for footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.getElementById("currentYear").innerText = new Date().getFullYear();
 document.getElementById("actionButton").addEventListener("click", function() {
     alert("Button clicked!");
 });
