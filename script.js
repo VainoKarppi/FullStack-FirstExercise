3b4820ce49380b9e2a6ca6ca13177794344bf84e
Väinö Karppi <vaino.karppi@gmail.com>
2024-01-08 11:21:38 +0200

    ADDED: Toggle description show when clicked from "Click me!"

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.getElementById("currentYear").innerText = new Date().getFullYear();
+
 document.getElementById("actionButton").addEventListener("click", function() {
     alert("Button clicked!");
+    ToggleDescription();
 });
+
+
+function ToggleDescription() {
+    var paragraph = document.getElementById("description");
+    paragraph.style.display = (paragraph.style.display === "none" || paragraph.style.display === "") ? "block" : "none";
+}
